375595fc60fb989a6cb3dae5f0eb5ee5ad28ca92
seungyun-Park <tmdrbs0925@gmail.com>
2024-05-17 01:13:29 +0900

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../seo.config';
 
@@ -7,6 +8,25 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <Script
+          id = "gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'GTM-W2FZMJ2H', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=GTM-W2FZMJ2H`}
+      />
       <Component {...pageProps} />
     </>
   );
